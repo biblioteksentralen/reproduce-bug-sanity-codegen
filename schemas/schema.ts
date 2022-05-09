f08163b837e0a9729e8dec3ba95305f2eab0f650
Daniel Winsvold <danielwinsvold@gmail.com>
2022-05-09 15:25:26 +0200

    not able to reproduce customInput bug

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -3,11 +3,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import schemaWithCustomInputcomponent from './schemaWithCustomInputcomponent'
 import schemaWithOrderableField from './schemaWithOrderableField'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([schemaWithCustomInputcomponent, schemaWithOrderableField]),
+  types: schemaTypes.concat([ schemaWithOrderableField]),
 })
